a6f24839414e3e28f1743b523d17b333062f0354
czyt <czytcn@gmail.com>
2026-03-18 22:22:55 +0800

    fix connect string

--- internal/database/database.go
+++ internal/database/database.go
@@ -11,7 +11,7 @@ import (
 
 func New(dsn string) (*ent.Client, error) {
 	// 使用文件模式，支持WAL和更好的并发
-	connStr := fmt.Sprintf("file:%s?cache=shared&_fk=1&_journal_mode=WAL&_synchronous=NORMAL&_busy_timeout=10000", dsn)
+	connStr := fmt.Sprintf("file:%s?cache=shared&_pragma=foreign_keys(1)&_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)&_pragma=busy_timeout(10000)", dsn)
 	client, err := ent.Open("sqlite3", connStr)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database: %w", err)
